0bd1e2eeb4ca1666a9fa61933eb8897e7695a9c4
Lê Thuý Hà <cobala869@gmail.com>
2025-05-12 04:17:54 +0700

    update when enemy hurt by flame

--- src/uet/oop/bomberman/entities/character/enemy/Enemy.java
+++ src/uet/oop/bomberman/entities/character/enemy/Enemy.java
@@ -1,6 +1,11 @@
 package uet.oop.bomberman.entities.character.enemy;
+import javafx.application.Platform;
 import javafx.scene.image.Image;
+import uet.oop.bomberman.BombermanGame;
+import static uet.oop.bomberman.BombermanGame.enemies;
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.sound.Sound;
 
 public abstract class Enemy extends Entity {
     // protected Entity old_cur = null;
