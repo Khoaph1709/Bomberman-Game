8053417984eb0cb3f417dbe7f8ba5e57ebaff7db
daisy <daissy2812@gmail.com>
2025-05-12 16:10:42 +0700

    Sửa Balloom.java

--- src/uet/oop/bomberman/entities/character/enemy/Balloom.java
+++ src/uet/oop/bomberman/entities/character/enemy/Balloom.java
@@ -23,4 +23,64 @@ public class Balloom extends Enemy {
             img = Sprite.movingSprite(Sprite.balloom_dead, Sprite.mob_dead1, Sprite.mob_dead2, Sprite.mob_dead3, animate, 20).getFxImage();
             return;
         }
+
+        switch (direction) {
+            case D:
+                if (checkWall(x, y + STEP + Sprite.SCALED_SIZE - 1) && checkWall(x + Sprite.SCALED_SIZE - 1, y + STEP + Sprite.SCALED_SIZE - 1)) {
+                    y += STEP;
+                    moving = true;
+                    if (moving) {
+                        sprite = Sprite.movingSprite(Sprite.balloom_right1, Sprite.balloom_right2, Sprite.balloom_right3, animate, 20);
+                    }
+                }
+                break;
+            case U:
+                if (checkWall(x, y - STEP) && checkWall(x + Sprite.SCALED_SIZE - 1, y - STEP)) {
+                    y -= STEP;
+                    moving = true;
+                    if (moving) {
+                        sprite = Sprite.movingSprite(Sprite.balloom_left1, Sprite.balloom_left2, Sprite.balloom_left3, animate, 20);
+                    }
+                }
+                break;
+            case L:
+                if (checkWall(x - STEP, y) && checkWall(x - STEP, y + Sprite.SCALED_SIZE - 1)) {
+                    x -= STEP;
+                    moving = true;
+                    if (moving) {
+                        sprite = Sprite.movingSprite(Sprite.balloom_left1, Sprite.balloom_left2, Sprite.balloom_left3, animate, 20);
+                    }
+                }
+                break;
+            case R:
+                if (checkWall(x + STEP + Sprite.SCALED_SIZE - 1, y) && checkWall(x + STEP + Sprite.SCALED_SIZE - 1, y + Sprite.SCALED_SIZE - 1)) {
+                    x += STEP;
+                    moving = true;
+                    if (moving) {
+                        sprite = Sprite.movingSprite(Sprite.balloom_right1, Sprite.balloom_right2, Sprite.balloom_right3, animate, 20);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+        img = sprite.getFxImage();
+        int new_px = getTileX();
+        int new_py = getTileY();
+        table[new_px][new_py] = this;
+    }
+
+    @Override
+    public void update() {
+        if (hurt) {
+            gotHurt(Sprite.balloom_dead);
+            return;
+        }
+        animate++;
+        moving = false;
+        findDirection();
+        balloomMoving();
+        checkCollideWithBomber();
+    }
 }
